95f7eb9f70f4dea69ac8032568065ad4d70e52d6
morganb27 <morgan.ballouk@gmail.com>
2024-04-30 20:20:33 +0200

    create new routes: /vote & /rankings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './routes/auth/auth.component';
 import { SignInComponent } from './core/components/sign-in/sign-in.component';
 import { SignUpComponent } from './core/components/sign-up/sign-up.component';
+import { VoteComponent } from './core/components/vote/vote.component';
+import { RankingsComponent } from './core/components/rankings/rankings.component';
 
 export const routes: Routes = [
 
@@ -16,4 +18,8 @@ export const routes: Routes = [
             { path: 'sign-up', component: SignUpComponent },
         ],
     },
+
+    { path: 'vote', component: VoteComponent},
+
+    { path: 'rankings', component: RankingsComponent}
 ];
