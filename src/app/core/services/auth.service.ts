ad7b928b7c0f38d9bba9a6c726f53eaadf03cccc
morganb27 <morgan.ballouk@gmail.com>
2024-05-01 19:27:11 +0200

    fix(auth): fix typo in url

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -11,7 +11,7 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(email: string, password: string) {
-    return this.http.post<LoginRequest>("hhttps://spring-boot-catmash.fly.dev/auth/authenticate", { email, password });
+    return this.http.post<LoginRequest>("https://spring-boot-catmash.fly.dev/auth/authenticate", { email, password });
   }
 
   register(firstname: string, lastname: string, email: string, password: string) {
