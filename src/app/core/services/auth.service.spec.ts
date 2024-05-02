b3d6526751897184d85415861f5a15730e0f8919
morganb27 <morgan.ballouk@gmail.com>
2024-05-02 17:30:58 +0200

    test(authService): test login method is auth.service

--- src/app/core/services/auth.service.spec.ts
+++ src/app/core/services/auth.service.spec.ts
@@ -1,15 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      providers: [AuthService]
+      providers: [AuthService,
+        { provide: HttpClient, useValue: httpClientSpy }
+      ]
     });
     service = TestBed.inject(AuthService);
   });
@@ -17,4 +22,30 @@ describe('AuthService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('When the login method is called', () => {
+    let expectedResponse = { token: '123456' };
+
+    beforeEach(() => {
+      httpClientSpy.post.and.returnValue(of(expectedResponse));
+    })
+
+    it('it should return a token', () => {
+      service.login('test@example.com', '12345').subscribe(response => {
+        expect(response).toEqual(expectedResponse);
+      });
+    })
+    it('should call HttpClient.post once', () => {
+      service.login('test@example.com', '12345').subscribe();
+      expect(httpClientSpy.post.calls.count()).toBe(1);
+    })
+    it('should call HttpClient.post with correct URL', () => {
+      service.login('test@example.com', '12345').subscribe();
+      expect(httpClientSpy.post.calls.mostRecent().args[0]).toBe('https://spring-boot-catmash.fly.dev/auth/authenticate');
+    })
+    it('should call HttpClient.post with correct credentials', () => {
+      service.login('test@example.com', '12345').subscribe();
+      expect(httpClientSpy.post.calls.mostRecent().args[1]).toEqual({ email: 'test@example.com', password: '12345' })
+    })
+  })
 });
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginRequest } from '../models/loginRequest';
 import { RegisterRequest } from '../models/registerRequest';
+import { Observable } from 'rxjs';
+import { LoginResponse } from '../models/loginResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +12,8 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  login(email: string, password: string) {
-    return this.http.post<LoginRequest>("https://spring-boot-catmash.fly.dev/auth/authenticate", { email, password });
+  login(email: string, password: string): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>("https://spring-boot-catmash.fly.dev/auth/authenticate", { email, password });
   }
 
   register(firstname: string, lastname: string, email: string, password: string) {
