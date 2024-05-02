db443996681d6fca693966bdbcf4111ecf48e5cc
morganb27 <morgan.ballouk@gmail.com>
2024-05-02 18:28:50 +0200

    test(service): unit tests for catService

--- src/app/core/models/cat.ts
+++ src/app/core/models/cat.ts
@@ -2,5 +2,5 @@ export interface Cat {
     id: number,
     name: String,
     imageURL: String,
-    votes: 0
+    votes: number
 }
--- src/app/core/services/cat.service.spec.ts
+++ src/app/core/services/cat.service.spec.ts
@@ -2,6 +2,8 @@ import { TestBed } from '@angular/core/testing';
 
 import { CatService } from './cat.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Cat } from '../models/cat';
+import { of } from 'rxjs';
 
 describe('CatService', () => {
   let service: CatService;
@@ -22,4 +24,27 @@ describe('CatService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('When the getCats method is called', () => {
+    let expectedCats: Cat[];
+
+    beforeEach(() => {
+      expectedCats = [{ id: 1, name: 'Tom', imageURL: 'http://example.com/cat-image', votes: 0 }, 
+      { id: 1, name: 'Jerry', imageURL: 'http://example.com/cat-image', votes: 1 }];
+      httpClientSpy.get.and.returnValue(of(expectedCats));
+    })
+    it('should return an array of cats', () => {
+      service.getCats().subscribe(cats => {
+        expect(cats).toEqual(expectedCats);
+      })
+    })
+    it('should call HttpClient.get once', () => {
+      service.getCats().subscribe();
+      expect(httpClientSpy.get.calls.count()).toBe(1);
+    });
+    it('should call HttpClient.get with correct URL', () => {
+      service.getCats().subscribe();
+      expect(httpClientSpy.get.calls.mostRecent().args[0]).toBe('https://spring-boot-catmash.fly.dev/cats');
+    });
+  })
 });
