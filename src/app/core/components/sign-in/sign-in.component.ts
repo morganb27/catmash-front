d5f0f75207ad21e96e431fa09f306c75d9d64fcb
morganb27 <morgan.ballouk@gmail.com>
2024-05-03 11:30:57 +0200

    chore(auth): improve overall design & UX/UI for when the user authenticate

--- src/app/core/components/sign-in/sign-in.component.ts
+++ src/app/core/components/sign-in/sign-in.component.ts
@@ -3,16 +3,18 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router, RouterLink } from '@angular/router';
+import { LoaderComponent } from '../loader/loader.component';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, LoaderComponent],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
   signInForm!: FormGroup;
+  isLoading = false;
 
   constructor(private authService: AuthService, private router: Router) { }
 
@@ -28,13 +30,17 @@ export class SignInComponent {
     const { email, password } = this.signInForm.value;
     this.authService.login(email, password).subscribe({
       next: (response: any) => {
+        this.isLoading = true;
         console.log('Login successful!', response);
         localStorage.setItem('token', response.token);
-        this.router.navigate(['/vote'])
+        setTimeout(() => {
+          this.router.navigate(['/vote'])
+        }, 2500);
       }
     })
   } else {
     console.error('Form is not valid.');
+    this.isLoading = false;
     this.signInForm.markAllAsTouched();
   }
  }
